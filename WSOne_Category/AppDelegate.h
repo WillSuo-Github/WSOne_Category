c626b997820c1bce5c224b5c234c01f69ec4cfc3
WS <a739002611@outlook.com>
2017-04-09 22:54:44 +0800

    1

---
+++ WSOne_Category/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WSOne_Category
+//
+//  Created by WS on 2017/4/9.
+//  Copyright © 2017年 WS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WSOne_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WSOne_Category
+//
+//  Created by WS on 2017/4/9.
+//  Copyright © 2017年 WS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
